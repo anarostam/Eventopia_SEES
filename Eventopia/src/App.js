cd69be8d6b0330e73a74176681c02b38d6914775
Ana Rostam <anna_rostam@yahoo.com>
2025-03-17 12:39:25 -0400

    updated

--- Eventopia/src/App.js
+++ Eventopia/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { restoreSession, getCurrentSession, supabase} from "./Client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import protectedRoute from "./components/protectedRoute";
+// import {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// } from "./pages";
+const App = () => {
+  const [token, setToken] = useState(false);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      const {
+        data: { session },
+        error,
+      } = await supabase.auth.getSession();
+
+      if(error){
+        console.error("Session Error:", error.message);
+        setToken(false);
+      } else if(session){
+        console.log("Session Found:", session);
+        setToken(true);
+      } else {
+        console.log("No session found.");
+        setToken(false);
+      }
+    };
+
+    checkSession();
+  }, []);
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    setToken(false);
+  };
+  // return(
+  //   <Routes>
+  //     {/* <Route path = "/login" element={<Login  setToken = {setToken/>} /> */}
+  //     {/*Public Routes*/}
+  //   </Routes>
+  // );
+  };
+
 
 export default App;
