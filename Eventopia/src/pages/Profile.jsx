8405aa53a10e3ed8d4adac06708cbaa3bfd8587d
Yash Nathani <y_nathan@live.concordia.ca>
2025-03-19 18:04:40 -0400

    Website Layout

---
+++ Eventopia/src/pages/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Css-folder/index.css'
+
+const Profile = () => {
+  return (
+    <div className="s-container">
+        <h1>Welcome to my profile!</h1>
+    </div>
+  )
+}
+
+export default Profile
