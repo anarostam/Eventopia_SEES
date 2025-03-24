52bb5b4756b75d347239574c95fd95f7f8f76733
khujista-01 <faqiri.khujista@gmail.com>
2025-03-24 19:25:45 -0400

    login-set-up

--- Eventopia/src/App.js
+++ Eventopia/src/App.js
@@ -1,3 +1,49 @@
+// import './Css-folder/App.css';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Header from './components/Header';
+// import Profile from './pages/Profile';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+// import Home from './pages/Home';
+// import Footer from './components/Footer';
+// import AddEvent from './pages/AddEvent';
+// import EventConfirmation from './pages/EventConfirmation';
+// import AddVenue from './pages/AddVenue';
+// import VenueConfirmation from './pages/VenueConfirmation';
+// import ViewEvent from './pages/ViewEvent';
+  
+
+// function App(){
+
+//   return (
+    
+//     <Router>
+//     <Header />
+//     <Routes>
+//       <Route path="/login" element={<Login />} />
+//       <Route path="/register" element={<Register />} />
+//       <Route path="/" element={<Home />} />
+//       <Route path="/profile" element={<Profile />} />
+//       <Route path="/addEvent" element={<AddEvent />} />
+//       <Route path="/eventConfirmation" element={<EventConfirmation />} />
+//       <Route path="/addVenue" element={<AddVenue />} />
+//       <Route path ="/venueConfirmation" element={<VenueConfirmation />} />
+//       <Route path="/ViewEvent" element={<ViewEvent />} />
+//       <Route path="/login" element={<Login setUser={setUser} />} />
+// <Route path="/profile" element={<Profile />} />
+
+//     </Routes>
+//     <Footer />
+//   </Router>
+//   );
+//   };
+
+
+// export default App;
+
+
+
+import React, { useState, useEffect } from 'react';
 import './Css-folder/App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
@@ -11,29 +57,34 @@ import EventConfirmation from './pages/EventConfirmation';
 import AddVenue from './pages/AddVenue';
 import VenueConfirmation from './pages/VenueConfirmation';
 import ViewEvent from './pages/ViewEvent';
-  
 
-function App(){
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
 
   return (
-    
     <Router>
-    <Header />
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/addEvent" element={<AddEvent />} />
-      <Route path="/eventConfirmation" element={<EventConfirmation />} />
-      <Route path="/addVenue" element={<AddVenue />} />
-      <Route path ="/venueConfirmation" element={<VenueConfirmation />} />
-      <Route path="/ViewEvent" element={<ViewEvent />} />
-    </Routes>
-    <Footer />
-  </Router>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/addEvent" element={<AddEvent />} />
+        <Route path="/eventConfirmation" element={<EventConfirmation />} />
+        <Route path="/addVenue" element={<AddVenue />} />
+        <Route path="/venueConfirmation" element={<VenueConfirmation />} />
+        <Route path="/ViewEvent" element={<ViewEvent />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
-  };
-
+}
 
 export default App;
