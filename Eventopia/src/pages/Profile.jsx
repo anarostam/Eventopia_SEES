4058844fd8770553a9cdee9926e594ea8f1f9f64
khujista-01 <faqiri.khujista@gmail.com>
2025-03-24 19:52:57 -0400

    updated login

--- Eventopia/src/pages/Profile.jsx
+++ Eventopia/src/pages/Profile.jsx
@@ -1,39 +1,3 @@
-// import React from 'react';
-// import '../Css-folder/index.css';
-// import { Link } from 'react-router-dom';
-
-// const Profile = () => {
-//   return (
-//     <div className="s-container">
-//       <h1>Welcome to my profile!</h1>
-
-//       <br />
-//       <br />
-//       <br />
-
-//       <div className="btn-group">
-//         <Link to="/AddEvent">
-//           <button className="btn btn-primary active" aria-current="page">Add a Event</button>
-//         </Link>
-//         <Link to="/ViewEvent">
-//         <button className="btn btn-primary">View Event</button>
-//         </Link>
-//         <Link to="/AddVenue">
-//         <button className="btn btn-primary"> Add a Venue</button>
-//         </Link>
-//       </div>
-
-//       <br />
-//       <br />
-//       <br />
-
-//     </div>
-//   );
-// };
-
-// export default Profile;
-
-
 
 import React, { useEffect, useState } from "react";
 import "../Css-folder/index.css";
