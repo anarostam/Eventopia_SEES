86015b1875b51ba142b46be2ce8e07f3ad7d2ca7
Yash Nathani <y_nathan@live.concordia.ca>
2025-03-26 23:24:10 -0400

    Login button UI and profile image alignment is fixed

--- Eventopia/src/components/Header.jsx
+++ Eventopia/src/components/Header.jsx
@@ -1,34 +1,48 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Header = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
+    // Check if the user is logged in
+    const user = JSON.parse(localStorage.getItem('user'));
+    const profileImage = localStorage.getItem("profileImage");
 
     const logoutClick = () => {
-        navigate('/login')
-    }
+        localStorage.removeItem('user'); 
+        localStorage.removeItem("profileImage"); 
+        navigate('/login'); 
+    };
 
-  return (
-    <div className="header">
-        <div>
-            <Link id="header-logo" to="/">Eventopia</Link>
+    return (
+        <div className="header">
+            <div>
+                <Link id="header-logo" to="/">Eventopia</Link>
+            </div>
+
+            <div className="links--wrapper">
+                <>
+                    <Link to="/" className="header--link">Home</Link>
+
+                    {user ? (
+                        <>
+                            <Link to="/profile" className="header--link">Profile</Link>
+                            <button onClick={logoutClick} className="btn">Log Out</button>
+
+                            <img
+                                src={profileImage || 'https://via.placeholder.com/50'}
+                                alt="Profile"
+                                className="profile-image-header"
+                                style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+                            />
+                        </>
+                    ) : (
+                        <Link className="btn" to="/login">Log In</Link> 
+                    )}
+                </>
+            </div>
         </div>
+    );
+};
 
-        <div className="links--wrapper">
-            <>
-                <Link to="/" className="header--link">Home</Link>
-                <Link to="/profile" className="header--link">Profile</Link>
-
-                <button onClick={logoutClick} className="btn">Log In</button>
-            </>
-            {/* <>
-
-                <Link className="btn" to="/login">Login</Link>
-            </> */}
-            
-        </div>
-    </div>
-  )
-}
-
-export default Header
+export default Header;
