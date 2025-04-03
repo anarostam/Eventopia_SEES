7ce18879c4fb91a5ed2dd9139c6c302545a15ce2
Yash Nathani <y_nathan@live.concordia.ca>
2025-04-03 17:24:58 -0400

    refactor the code structure

--- Eventopia/src/pages/Event/AddEvent.jsx
+++ Eventopia/src/pages/Event/AddEvent.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 //import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-import '../Css-folder/AddEvent.css';
-import { AddEventBack} from "./AddEventBack";
+import '../../Css-folder/AddEvent.css';
+import { AddEventBack} from "../backend/AddEventBack";
 
-import { supabase } from "../Client";
+import { supabase } from "../../Client";
 
 const AddEvent = () => {
   const [eventName, setEventName] = useState('');
